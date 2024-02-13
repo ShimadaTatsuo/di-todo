f782f30dc0048735e1ada8da181cd67160d0ac3b
島田龍男 <tatsuo.shimada39@gmail.com>
2024-02-13 16:16:31 +0900

    add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { container } from "./styles.css.ts";
+
 function App() {
   return (
     <>
-      <h1>di-todo</h1>
+      <section className={container}>
+        <h1>di-todo</h1>
+      </section>
     </>
   );
 }
