a799d14d6d12aae405d4d060a8f5de8a807bb42c
島田龍男 <tatsuo.shimada39@gmail.com>
2024-02-19 10:32:24 +0900

    fix: hide router-devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,8 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Section } from "../components/ui/Section/Section";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 export const Route = createRootRoute({
   component: () => {
     return (
@@ -19,7 +21,7 @@ export const Route = createRootRoute({
 
         <Outlet />
 
-        <TanStackRouterDevtools />
+        {isDevelopment && <TanStackRouterDevtools />}
       </>
     );
   },
